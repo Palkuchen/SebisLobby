80d3caaa74443848cf0e299d779dfcf6900f462b
Palkuchen <palkuchen@gmail.com>
2024-10-19 23:07:32 +0200

    Inital Commit

---
+++ src/main/java/de/palkuchen/kuchencraft/sebislobby/SebisLobby.java
@@ -0,0 +1,17 @@
+package de.palkuchen.kuchencraft.sebislobby;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SebisLobby extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
